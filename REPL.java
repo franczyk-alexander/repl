2d736c18382a445d48f3a2f142c05a406baa4d89
Alexander Franczyk <franczykalexander@gmail.com>
2018-10-17 20:15:55 -0400

    first commit

---
+++ REPL.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.io.IOException;
+import java.lang.Math;
+
+public class REPL {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            System.out.print("> ");
+            String input = scan.nextLine();
+            String[] arguments = input.split(" ");
+
+            if (arguments[0].equals("exit") || arguments[0].equals("quit")) {
+                break;
+            }
+
+            if (arguments[0].equals("gcf")) {
+                if (arguments.length != 3 || !isInteger(arguments[1]) || !isInteger(arguments[2])) {
+                    System.out.println("usage: gcf [multiple] [multiple]");
+                } else {
+                    System.out.println(gcf(Integer.valueOf(arguments[1]), Integer.valueOf(arguments[2])));
+                }
+            }
+            else if (arguments[0].equals("lcm")) {
+                if (arguments.length != 3 || !isInteger(arguments[1]) || !isInteger(arguments[2])) {
+                    System.out.println("usage: lcm [factor] [factor]");
+                } else {
+                    System.out.println(lcm(Integer.valueOf(arguments[1]), Integer.valueOf(arguments[2])));
+                }
+            }
+            else if (arguments[0].equals("clear")) {
+                clearScreen();
+            }
+            else {
+                System.out.println("unknown operation \"" + arguments[0] + "\"");
+            }
+        }
+    }
+
+    private static int gcf(int x, int y) {
+        if (y == 0) {
+            return x;
+        } else {
+            return gcf(y, x % y);
+        }
+    }
+
+    private static int lcm(int x, int y) {
+        return Math.abs(x * y) / gcf(x, y);
+    }
+
+    private static boolean isInteger(String integer) {
+        try {
+            Integer.valueOf(integer);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+
+    private static void clearScreen() {
+        String os = System.getProperty("os.name");
+        try {
+            if (os.toLowerCase().contains("windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            }
+        } catch (IOException | InterruptedException e) {
+            //
+        }
+    }
+}
